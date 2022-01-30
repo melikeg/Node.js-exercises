5cbe91abe96cd13548f007cbbdcbfab68d17d6c4
Melike Görgülü <45533057+melikeg@users.noreply.github.com>
2022-01-30 13:46:37 +0300

    Update Odev4-fileSystem.js

--- Odev4-fileSystem.js
+++ Odev4-fileSystem.js
@@ -35,8 +35,11 @@ file.salary = "3500";
 
 fs.writeFile(fileName, JSON.stringify(file), function writeJSON(err) {
   if (err) return console.log(err);
-  console.log(JSON.stringify(file));
-  console.log("writing to " + fileName);
+  else{
+    console.log(JSON.stringify(file));
+    console.log("writing to " + fileName);
+    console.log("The file has been updated.");
+  }
 });
 
 //Dosyayı silelim.
